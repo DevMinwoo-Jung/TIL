dda0799f1b417f8596abc7c04d3132128273944b
minwoojung <minwoojung@zaisoft.co.kr>
2024-01-10 15:28:18 +0900

    rese paramter

--- 2024/JS/promise.js
+++ 2024/JS/promise.js
@@ -2,9 +2,22 @@ function delay(ms) {
     // 여기에 코드 작성
     return new Promise((resolve, reject) => {
         setTimeout(()=>{
-            resolve()
+            resolve(ms)
         }, ms)
     })
   }
   
-  delay(3000).then(() => alert('3초후 실행'));
+//  delay(3000).then(() => alert('3초후 실행'));
+
+// delay(3000).then((result) => console.log(result));
+
+
+function argumentsTest(...args){
+    console.log(...args)
+    console.log(args)
+    console.log(args[0])
+
+};
+
+argumentsTest('1','2')
+argumentsTest('1')
