d1c13e2ebdb49d7d95175b41d8488f210b89d4e8
minwoojung <minwoojung@zaisoft.co.kr>
2024-01-10 17:26:31 +0900

    prototype

--- 2024/JS/promise.js
+++ 2024/JS/promise.js
@@ -21,3 +21,56 @@ function argumentsTest(...args){
 
 argumentsTest('1','2')
 argumentsTest('1')
+
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("asdasdasd");
+    }, 300);
+    // 여기서 통신요청을 한 다음에 그 결과값을 resolve에 담은다음에 체이닝 하면 되나??
+  });
+  
+  myPromise
+  .then((value) => `${value} 1`)
+  .then((value) => `${value} 2`)
+  .then((value) => `${value} 3`)
+  .then((value) => `${value} 4`)
+  .then((value) => {
+    console.log(value);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+let message = "Hello!";
+let phrase = message;
+
+
+console.log(message === phrase);
+
+
+let user = { name: "John" };
+
+let admin = user; // 참조값을 복사함
+
+user.name = '바꿈';
+
+console.log(user,admin);
+
+
+function exampleObj() {
+    this.name = 'minwoo'
+}
+
+const exampleObjTest = new exampleObj();
+
+exampleObj.prototype.sayMyName = function(){
+    console.log(this) // exampleObj 
+    console.log(this.name)
+}
+
+// exampleObj.prototype.sayMyName =()=>{
+//     console.log(this) // window
+//     console.log(this.name)
+// }
+
+exampleObjTest.sayMyName();
